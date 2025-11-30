c7d6ddc7c854b622fdd692b0bfa85645a86b87ee
Sebastien Riou <sebastien.riou@pqshield.com>
2025-11-30 22:45:43 +0100

    remove stuff moved to dedicaed repo

--- testvectors-gen/hdrbg/sha.h
+++
@@ -1,9 +0,0 @@
-#ifndef TFPF_HASH_DRBG_INCLUDE_SHA_H_
-#define TFPF_HASH_DRBG_INCLUDE_SHA_H_
-
-#include <inttypes.h>
-#include <stddef.h>
-
-uint8_t *sha256(uint8_t const *message_, size_t length_, uint8_t *h_bytes);
-
-#endif  // TFPF_HASH_DRBG_INCLUDE_SHA_H_
